09c0fab7575a7730619d10cbdecfec0556ab6d25
Ibrahim Qandeel <ibrahim.s.qandeel@gmail.com>
2020-03-09 21:47:04 +0300

    using zipkin

--- src/main/java/com/microservice/ApiGatewayServerApplication.java
+++ src/main/java/com/microservice/ApiGatewayServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -14,4 +17,9 @@ public class ApiGatewayServerApplication {
 		SpringApplication.run(ApiGatewayServerApplication.class, args);
 	}
 
+	@Bean
+	// creating a sampler called
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
